204a2eaaee0393b54b6876aa8162913fca8d5bde
Gary R. Van Sickle <grvs@sourceforge.net>
2024-03-04 14:35:43 -0600

    Initial add concurrency/realtime.h/.cpp.

---
+++ concurrency/realtime.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of grvslib.
+ *
+ * grvslib is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * grvslib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * grvslib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file Concurrency classes and functions which are mostly useful in real-time situations.
+ */
+
+#ifndef GRVSLIB_REALTIME_H
+#define GRVSLIB_REALTIME_H
+
+#include <atomic>
+
+class lock_free_atomic_data_transfer
+{
+public:
+
+private:
+	std::atomic_flag m_has_been_updated = ATOMIC_FLAG_INIT;
+};
+
+
+#endif //GRVSLIB_REALTIME_H
