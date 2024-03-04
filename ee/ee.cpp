ea08d7be05f242cc5dd47a99be95ccc13c606b67
Gary R. Van Sickle <grvs@sourceforge.net>
2024-03-04 11:49:26 -0600

    Initial add ee.h/.cpp.

---
+++ ee/ee.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2024 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of grvslib.
+ *
+ * grvslib is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * grvslib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * grvslib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ee.h"
