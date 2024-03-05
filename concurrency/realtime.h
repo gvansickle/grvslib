0f75c9500f95d988215881a31e56672f6686f54e
Gary R. Van Sickle <grvs@sourceforge.net>
2024-03-05 16:37:07 -0600

    lock_free_atomic_data_transfer work.

--- concurrency/realtime.h
+++ concurrency/realtime.h
@@ -30,17 +30,49 @@ class lock_free_atomic_data_transfer
 {
 public:
 
-	void update_and_clear_if_set(PayloadType* reader_payload)
+	bool update_and_clear_if_set(PayloadType* reader_payload)
 	{
 		if(m_has_been_updated.test())
 		{
-			///...@todo
+			// The payload has been updated.  Let's read the value.
+			while(m_is_being_accessed.test_and_set())
+			{
+				while(m_is_being_accessed.test())
+				{
+					// Spin.
+				}
+			}
+			// We've got the m_is_being_accessed lock here.
+
+			*reader_payload = m_payload;
+
+			// Clear the update notification flag.
+			m_has_been_updated.clear();
+			// Unblock any threads which may be waiting in set().
+			m_is_being_accessed.notify_all();
+
+			// Indicate that we did a data transfer.
+			return true;
 		}
+
+		// Indicate that no data was transferred.
+		return false;
 	}
 
-	void set()
+	void set(const PayloadType* new_writer_payload)
 	{
-		///...@todo
+		// Wait until the payload lock is false.
+		m_is_being_accessed.wait(true);
+		m_is_being_accessed.test_and_set();
+
+		m_payload = *new_writer_payload;
+
+		// Clear the payload lock.
+		m_is_being_accessed.clear();
+		// In general, nobody should be waiting for us, but notify just in case.
+		m_is_being_accessed.notify_all();
+
+		// Set the update notification flag.
 		m_has_been_updated.test_and_set();
 	}
 private:
@@ -50,6 +82,8 @@ private:
 	 *       minimal-pain backward compatibility.
 	 */
 	std::atomic_flag m_has_been_updated = ATOMIC_FLAG_INIT;
+	std::atomic_flag m_is_being_accessed = ATOMIC_FLAG_INIT;
+	PayloadType m_payload;
 };
 
 
