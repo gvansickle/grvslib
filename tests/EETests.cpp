4ccc0fda945b09573b3daf2ddc8cd47bda0c4f3f
Gary R. Van Sickle <grvs@sourceforge.net>
2024-03-10 14:04:42 -0500

    Added some tests, gtest is running.

---
+++ tests/EETests.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2024 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of grvslib.
+ *
+ * grvslib is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * grvslib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * grvslib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file Tests for the ee subdirectory.
+ */
+
+#include <gtest/gtest.h>
+
+#include <ee/basic_calculators.h>
+
+TEST(EE_BasicCalculators, rpar1) {
+	EXPECT_DOUBLE_EQ(5000.0, rpar(10'000.0, 10'000.0));
+}
+
+TEST(EE_BasicCalculators, rdiv_to_gain1)
+{
+	EXPECT_FLOAT_EQ(0.5f, rdiv_to_gain(20'000.0f, 20'000.0f));
+}
